c447ee3105b2f8b55e57d6c17ccfbb3505d3d361
minhdt <minhdthe163918@fpt.edu.vn>
2023-10-23 13:11:26 +0700

    home + DETAIL

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-
+import { Col, Container, Row } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Detail from "./Detail";
 
 function App() {
   return (
-    <>
-    </>
+    <Container fluid>
+    <BrowserRouter>
+      <Row>
+        <Col style={{ textAlign: "center" }}>
+          <h2>Project FER</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+        <Container fluid>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/home/:id/detail" element={<Detail/>} />
+            <Route path="/detail" element={<Detail/>} />
+          </Routes>
+          </Container>
+        </Col>
+      </Row>
+    </BrowserRouter>
+    </Container>
   );
 }
 
